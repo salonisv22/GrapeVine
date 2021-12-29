340dbe3e15fc41bb78226c74b8fbb16313d13d87
salonisv22 <77237060+salonisv22@users.noreply.github.com>
2021-12-29 22:10:36 +0530

    fixed question > views.py

--- backend/question/models.py
+++ backend/question/models.py
@@ -6,6 +6,7 @@ class Question(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     questioned_at = models.DateTimeField(auto_now_add = True)
     questioned_by = models.ForeignKey(Users, on_delete = models.CASCADE)
+    query_title = models.CharField(max_length=50)
     query = models.TextField()
     upvotes = models.IntegerField(default = 0)
     downvotes = models.IntegerField(default = 0)
