fa0a83fb627c1378341c0c514979bace3d9e881f
ankitkumarbrur <ak248100@gmail.com>
2021-12-30 23:38:45 +0530

    Fix: user field in questions model

--- backend/question/serializers.py
+++ backend/question/serializers.py
@@ -2,9 +2,10 @@ from rest_framework import serializers
 from .models import Question
 
 class QuestionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Question
         fields = "__all__"
-
+        read_only_fields = [
+            'user'
+        ]
     
